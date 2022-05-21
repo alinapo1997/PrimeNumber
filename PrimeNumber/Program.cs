8924679454c5dc1b36d3c6c7e7601da7d8b9811c
Alina Podvalnaia <alinapo1997@gmail.com>
2022-05-21 17:53:03 +0400

    Добавьте файлы проекта.

---
+++ PrimeNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PrimeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {            
+            Console.WriteLine("Введите целое число: ");
+            int unknownNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("___________________START___________________");
+            bool isSimple = true;
+            int i = 1;
+            while (i < unknownNumber)
+            {                 
+                int remainder = unknownNumber % i;                
+                if (i != 1 && remainder == 0)
+                {
+                    isSimple = false;                 
+                }
+                i++;            
+            }
+            string result = isSimple ? "Число простое" : "Число не простое";
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
